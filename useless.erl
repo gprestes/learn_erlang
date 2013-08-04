68007d407162be9b033415e96185741d8db5b20b
Guilherme Heuser Prestes <guilherme.prestes@gmail.com>
2013-08-04 03:40:51 -0300

    created 1st Erlang script: useless

---
+++ useless.erl
@@ -0,0 +1,17 @@
+-module(useless).
+-author("Guilherme Heuser Prestes").
+
+-compile([debug_info, export_all]).
+-export([add/2, hello/0, greet_and_add_two/1]).
+
+add(A, B) ->
+  A + B.
+
+% Shows greetings.
+% io:format/1 is the standard function used to output text.
+hello() ->
+  io:format("Hello world!~n").
+
+greet_and_add_two(X) ->
+  hello(),
+  add(X,2).
